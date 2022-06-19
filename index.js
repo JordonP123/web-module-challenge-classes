746f8c524e863c66cfaa58eb1a2a5dac926f4193
JordonP123 <jordonaren@gmail.com>
2022-06-19 03:27:55 -0400

    DONE

--- index.js
+++ index.js
@@ -44,8 +44,32 @@ class Airplane {
 */
 
 class Person {
+  constructor(name, age){
+    this.name = name;
+    this.age = age;
+    this.stomach = [];
+  }
+  eat(edible){
+      if(this.stomach.length < 10){
+        this.stomach.push(edible)
+      }
+    }
+    poop(){
+      this.stomach = []
+    }
+
+    toString(){
+    return `${this.name}, ${this.age}`
+    }
+  }
+
   
-}
+
+
+
+
+ 
+  
 
 /*
   TASK 2
@@ -62,8 +86,57 @@ class Person {
 */
 
 class Car {
+  constructor(model, milesPerGallon){
+    this.model = model
+    this.milesPerGallon = milesPerGallon
+    this.tank = 0
+    this.odometer = 0
+  }
+  fill(gallons){
+    this.tank += gallons
+  }
+ drive(distance){
+  const miles = this.tank * this.milesPerGallon;
+  if(distance <= miles){
+    this.odometer += distance
+    this.tank = this.tank - (distance/this.milesPerGallon)}
+    else{
+      this.odometer += miles;
+      this.tank = 0
+      return `I ran out of fuel at ${this.odometer} miles!`
+    }
   
-}
+ }
+ }
+
+
+// drive(distance){
+//   let a = this.odometer += distance
+//   let x = this.tank = this.tank - (distance/this.milesPerGallon)
+//   if(this.tank === 0){
+    
+    
+//   }
+
+//else to work if car can't drive..
+  
+      
+     
+
+    
+
+
+
+    // instance of Car
+  //   const car2 = new Car('nissan', 200)
+    
+
+  // car2.fill(200)
+  // console.log(car2)
+  // car2.drive(100)
+  // console.log(car2)
+    
+
 
 /*
   TASK 3
@@ -79,8 +152,22 @@ class Car {
 */
 
 class Lambdasian {
-  
+  constructor(obj){
+    this.name = obj.name;
+    this.location = obj.location;
+    this.age = obj.age;
+  }
+ speak(){
+  return `Hello my name is ${this.name}, I am from ${this.location}`
+ }
 }
+
+
+
+
+
+
+
 
 /*
   TASK 4
@@ -97,10 +184,21 @@ class Lambdasian {
         + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
 */
 
-class Instructor {
+class Instructor extends Lambdasian {
+    constructor(obj){
+      super(obj);
+      this.specialty = obj.specialty;
+      this.favLanguage = obj.favLanguage;
+      this.catchPhrase = obj.catchPhrase;
 
-}
-
+    }
+      demo(subject){
+        return `Today we are learning about ${subject}`
+      }
+      grade(obj, subject){
+        return `${obj.name} receives a perfect score on ${subject}`
+      }
+   }
 /*
   TASK 5
     - Write a Student class extending Lambdasian.
@@ -117,8 +215,23 @@ class Instructor {
         + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
 */
 
-class Student {
-   
+class Student extends Lambdasian {
+  constructor(obj){
+    super(obj)
+    this.previousBackground = obj.previousBackground;
+    this.className = obj.className;
+    this.favSubjects = obj.favSubjects;
+  }
+listSubjects(){
+  return `${this.favSubjects}`;
+}
+PRAssignment(subject){
+  return `${this.name} has submitted a PR for ${subject}`
+}
+sprintChallenge(subject){
+  return `${this.name} has begun sprint challenge on ${subject}`
+}
+
 }
 
 /*
@@ -135,9 +248,20 @@ class Student {
         + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
 */
 
-class ProjectManager {
-   
-}
+class ProjectManager extends Instructor {
+   constructor(obj){
+    super(obj);
+    this.gradClassName = obj.gradClassName;
+    this.favInstructor = obj.favInstructor;
+    }
+   standUp(channel){
+    return `${this.name} announces to ${channel}, @channel standy times!`
+    }
+    debugsCode(obj, subject){
+      return `${this.name} debugs ${obj.name}'s code on ${subject}`
+    }
+  }
+
 
 /*
   STRETCH PROBLEM (no tests!)
